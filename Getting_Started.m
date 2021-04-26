cb02a463a2f8070bfa4bed66894f642d9eb39242
Souvik Datta <souvikdatta123@gmail.com>
2021-04-26 13:13:15 +0530

    First Commit

--- Getting_Started.m
+++ Getting_Started.m
@@ -1,6 +1,6 @@
 clc 
 clear all 
-% A homogeneous transformation representing a pure translation
-% of 0.5 m in the X-direction is created with the following 
+% tform = trvec2tform(trvec) converts the Cartesian representation of a translation vector, trvec, to the corresponding homogeneous transformation, tform. When using the
+% transformation matrix, premultiply it with the coordinates to be transformed (as opposed to postmultiplying). 
 trvec = [0.5 6 100]
 tform = trvec2tform(trvec)
