135b59d3c55cba21d4693980e19473e035d8b31f
Souvik Datta <souvikdatta123@gmail.com>
2021-04-26 13:07:44 +0530

    Beginner

---
+++ Getting_Started.m
@@ -0,0 +1,6 @@
+clc 
+clear all 
+% A homogeneous transformation representing a pure translation
+% of 0.5 m in the X-direction is created with the following 
+trvec = [0.5 6 100]
+tform = trvec2tform(trvec)
