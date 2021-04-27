caa5622dd9ddd47945e8900682fb574edc3282d4
Souvik Datta <souvikdatta123@gmail.com>
2021-04-27 15:50:14 +0530

    Input Complex Code

--- path_finder_complex_route.m
+++ path_finder_complex_route.m
@@ -10,33 +10,35 @@ robotRadius = 0.2
 %load a more complex map
 map = binaryOccupancyMap(complexMap,1)
 show(map)
+
 mapInflated = copy(map)
 inflate(mapInflated, robotRadius);
 show(mapInflated)
-% prm = mobileRobotPRM;
-% prm.Map = mapInflated;
-% prm.NumNodes = 50;
-% prm.ConnectionDistance = 10;
-% startLocation = [3 1]
-% endLocation = [45 35]
-% %final path 
-% path = findpath(prm, startLocation, endLocation)
-% show(prm)
-% % For complex maps, there may not be a feasible
-% % path for a given number of nodes (returns an empty path).
-% path = findpath(prm, startLocation, endLocation);
-% while isempty(path)
-%  % No feasible path found yet, increase the number of nodes
-%  prm.NumNodes = prm.NumNodes + 10;
-% 
-%  % Use the |update| function to re-create the PRM roadmap with the changed
-%  % attribute
-%  update(prm);
-% 
-%  % Search for a feasible path with the updated PRM
-%  path = findpath(prm, startLocation, endLocation);
-% end
-% show(prm)
+prm = mobileRobotPRM;
+prm.Map = mapInflated;
+prm.NumNodes = 50;
+prm.ConnectionDistance = 10;
+startLocation = [3 1]
+endLocation = [45 35]
+%final path 
+path = findpath(prm, startLocation, endLocation)
+show(prm)
+
+% For complex maps, there may not be a feasible
+% path for a given number of nodes (returns an empty path).
+path = findpath(prm, startLocation, endLocation);
+while isempty(path)
+ % No feasible path found yet, increase the number of nodes
+ prm.NumNodes = prm.NumNodes + 10;
+
+ % Use the |update| function to re-create the PRM roadmap with the changed
+ % attribute
+ update(prm);
+
+ % Search for a feasible path with the updated PRM
+ path = findpath(prm, startLocation, endLocation);
+end
+show(prm)
 
 
 
